a36174fb6f0bfc13aca7e4c677fa6f4e784d1068
dhsin98 <shinjay417@gmail.com>
2023-11-22 11:17:33 +0900

    feat: router 설정

---
+++ src/pages/analyze/Analyze.js
@@ -0,0 +1,4 @@
+const Analyze = () => {
+  return <div>Analyze페이지 입니다.</div>;
+};
+export default Analyze;
---
+++ src/pages/main/Main.js
@@ -0,0 +1,4 @@
+const Main = () => {
+  return <div>main pag입니다</div>;
+};
+export default Main;
